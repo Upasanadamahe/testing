a00da6868386095bdc937ba54519006c1c2dd439
Upasanadamahe <86532429+Upasanadamahe@users.noreply.github.com>
2024-05-29 23:42:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region ="us-east-1"
+  
+}
+resource "aws_instance" "name" {
+     ami = "ami-0bb84b8ffd87024d8"
+     instance_type = "t2.micro"
+    key_name = new
+    tags = {
+        Name="jenkin-server"
+    }
+  
+ }
