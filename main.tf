49f3bb81ccd72ed5f664c06e0ad09747a2cb378e
Upasanadamahe <86532429+Upasanadamahe@users.noreply.github.com>
2024-05-30 00:04:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "name" {
      ami = "ami-0bb84b8ffd87024d8"
      instance_type = "t2.micro"
-    key_name = new
+    key_name = "new"
     tags = {
         Name="jenkin-server"
     }
