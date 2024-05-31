e933a71e0ba68aa7e93f51c2fddd5c06209898c2
Upasanadamahe <86532429+Upasanadamahe@users.noreply.github.com>
2024-05-31 08:01:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,2 @@
-provider "aws" {
-  region ="us-east-1"
-  
-}
-resource "aws_instance" "name" {
-     ami = "ami-0bb84b8ffd87024d8"
-     instance_type = "t2.micro"
-    key_name = "new"
-    tags = {
-        Name="jenkin-server"
-    }
-  
- }
+mkdir boss 
+touch file111
