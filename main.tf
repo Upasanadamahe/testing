ac8a50d77c20887d8c00d77be59ae7f171618708
Upasanadamahe <86532429+Upasanadamahe@users.noreply.github.com>
2024-05-31 15:49:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,11 @@
-mkdir boss 
-touch file111
+provider "aws"{
+region="us-east-1"
+}
+respurce "aws_instance" "s1"{
+ami ="ami-0bb84b8ffd87024d8"
+instance_type ="t2.micro"
+key_name="pvt"
+tag={
+Name ="s001"
+}
+}
